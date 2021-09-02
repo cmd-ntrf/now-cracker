dfc47902c47b481b46d6c7b4a538dd7fc2f922b4
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2021-09-02 10:17:49 -0400

    Replace thing array by tuple map

--- index.go
+++ index.go
@@ -11,49 +11,47 @@ import (
 	"time"
 )
 
-type thing struct {
+type tuple struct {
 	time time.Time
 	pass string
-	hash string
 }
 
 const PIN_LENGTH = 8
 const PIN_PADDING = "%08d"
 const MAX_PIN = 100000000
 
-var things = make([]thing, 0)
+var tuples = make(map[string]tuple)
 
-func filterOutOldThings(t time.Time) {
-	new_things := make([]thing, 0)
-	for _, thing := range things {
-		if thing.time.Add(time.Second * 2).After(t) {
-			new_things = append(new_things, thing)
+func filterOutOldTuples(t time.Time) {
+	new_tuples := make(map[string]tuple)
+	for key, tuple := range tuples {
+		if tuple.time.Add(time.Second * 2).After(t) {
+			new_tuples[key] = tuple
 		}
 	}
-	things = new_things
-	fmt.Printf("%v\n", things)
+	tuples = new_tuples
+	fmt.Printf("%v\n", tuples)
 }
 
 func addNewThing(t time.Time) string {
 	pass := fmt.Sprintf(PIN_PADDING, rand.Intn(MAX_PIN))
 	hash := sha256.Sum256([]byte(pass))
 	hexDigest := hex.EncodeToString(hash[:])
-	things = append(things, thing{t, pass, hexDigest})
+	tuples[hexDigest] = tuple{t, pass}
 	return hexDigest
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	now := time.Now()
-	filterOutOldThings(now)
+	filterOutOldTuples(now)
 	switch r.Method {
 	case "POST":
 		givenHash := r.URL.Path[1:]
 		body, _ := ioutil.ReadAll(r.Body)
 		givenPass := string(body)
-		for _, thing := range things {
-			if thing.hash == givenHash && thing.pass == givenPass {
-				fmt.Fprintln(w, os.Getenv("FLAG"))
-			}
+		tuple, ok := tuples[givenHash]
+		if ok && tuple.pass == givenPass {
+			fmt.Fprintln(w, os.Getenv("FLAG"))
 		}
 	case "GET":
 		hash := addNewThing(now)
